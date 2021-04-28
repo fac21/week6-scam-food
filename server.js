a58cd222bcb835fd702b5bfca5dbdbe44479c02d
Amy Kuner <amykuner1@gmail.com>
2021-04-28 16:20:22 +0100

    merging main

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const staticHandler = express.static("public");
 server.use(staticHandler);
 
 const homePage = require("./src/handlers/index");
+const signUpPage = require("./src/handlers/signUp");
 
 server.get("/", homePage.getHomepage)
 
-
+server.get("/sign-up", signUpPage.getSignUp)
 
  
 const PORT = 3000 || process.env.PORT;
