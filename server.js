6e2b27ca2ce5ce4266910c22e5a25795771408d7
chisha <aamaechi@hotmail.com>
2021-04-29 11:20:47 +0100

    Modified PORT variable for Heroku deployment setup

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.get("/", homePage.getHomepage)
 server.get("/sign-up", signUpPage.getSignUp)
 
  
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => { console.log(`Listening on http://localhost:${PORT}`)});
 
